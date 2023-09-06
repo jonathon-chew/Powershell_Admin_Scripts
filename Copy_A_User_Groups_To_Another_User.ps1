41f6bfe56a92f36bb4abf3f4d7fe5c69419876ff
Jonathon Chew <89095880+jonathon-chew@users.noreply.github.com>
2023-09-06 15:24:21 +0100

    Configured Copy User Groups to work!

--- Copy_A_User_Groups_To_Another_User.ps1
+++ Copy_A_User_Groups_To_Another_User.ps1
@@ -1,6 +1,6 @@
-$firstUser= Read-host "Whose groups would you like to copy? (Username) "
+function copyUserGroups(){$firstUser= Read-host "Whose groups would you like to copy? (Username) "
 $secondUser = Read-Host "Who would you like to copy the groups to? (Username) "
 $creds = Get-Credential
-$groups=(Get-ADUser $firstUser -Properties MemberOf).MemberOf)
+$groups=$groups=(Get-ADUser $firstUser -Properties MemberOf).memberof | Get-ADGroup | Select-Object name
 
-foreach ($group in $groups) { Add-ADGroupMember -identity $group.name -MemberOf $secondUser -Credential $creds}
+foreach ($group in $groups) { Add-ADGroupMember -identity $group.name -Members $secondUser -Credential $creds}}
