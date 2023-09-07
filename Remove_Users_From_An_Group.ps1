e8fe0aa017e2bb892ac672af06ecdf9f4aeaf3dc
Jonathon Chew <89095880+jonathon-chew@users.noreply.github.com>
2023-09-07 13:46:09 +0100

    Configured the read message to more accurately reflect what it is the user will be doing from the input

--- Remove_Users_From_An_Group.ps1
+++ Remove_Users_From_An_Group.ps1
@@ -1,3 +1,3 @@
-$group=read-host "What Group Would You Like To Add Them To?"; 
+$group=read-host "What Group Would You Like To Remove Them From?"; 
 $cred=Get-Credential; 
 Import-csv .\Remove_Users.csv | ForEach-Object {$userName=$($_.Users); write-host "Do you want to remove user: " $userName; $(remove-adgroupmember -identity $group -Members $userName -Credential $cred)}
